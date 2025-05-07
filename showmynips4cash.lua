1672ea168ca7305b4ad84fd1f54a5d2cbd6706e3
ngr <168592226+cuteasses@users.noreply.github.com>
2025-05-06 23:17:11 -0400

    Add files via upload

--- showmynips4cash.lua
+++ showmynips4cash.lua
@@ -1,5 +1,5 @@
 getgenv().DiscordWebhook = "WEBHOOK_DISCORD" -- optional
 getgenv().TimetoMessage = 2
-getgenv().Camerainvoid = false -- picking up cash more faster if keep "true"
+getgenv().Camerainvoid = true -- picking up cash more faster if keep "true"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/yuvic123/SKIDO-V3/refs/heads/main/farm"))()
